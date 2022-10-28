a9be1fecf4c6678c2030ee6399b5e0eb36879406
nexik <tomasz.slazok@landingi.com>
2022-10-28 15:58:34 +0200

    Env flags

--- main/public/index.php
+++ main/public/index.php
@@ -1,5 +1,5 @@
 <?php
-
+use Tbd\Main\FeatureFlags\EnvOverrider;
 use Tbd\Main\FeatureFlags\FeatureFlag;
 use Tbd\Main\FeatureFlags\InMemoryFeatureFlags;
 
@@ -7,6 +7,8 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $initialFlags = require __DIR__ . '/../src/Flags.php';
 $featureFlags = new InMemoryFeatureFlags($initialFlags);
+$envOverrider = new EnvOverrider();
+$featureFlags = new InMemoryFeatureFlags($envOverrider->overrideFlags($initialFlags));
 FeatureFlag::setFeatureFlags($featureFlags);
 
 $app = new \Tbd\Main\Application();
